bd2b101f17f9ccd9e2c0fe43a1bf052230d2d742
havababay <hava.babay@gmail.com>
2024-09-09 19:22:30 +0300

    connet angular application to firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,23 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding()), provideAnimations()]
+  providers: [
+    provideRouter(routes, withComponentInputBinding()),
+    provideAnimations(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'frontend-final-project-demo',
+        appId: '1:1022831664674:web:90178c74ec5861ba80347b',
+        storageBucket: 'frontend-final-project-demo.appspot.com',
+        apiKey: 'AIzaSyAjhvFYEJcED7CE61awd2R9XxEzU2IU6dU',
+        authDomain: 'frontend-final-project-demo.firebaseapp.com',
+        messagingSenderId: '1022831664674',
+      })
+    ),
+    provideFirestore(() => getFirestore()),
+  ],
 };
--- src/app/choose-team-dialog/choose-team-dialog.component.ts
+++ src/app/choose-team-dialog/choose-team-dialog.component.ts
@@ -1,19 +1,47 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Inject, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, Input, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { Task } from '../shared/model/task';
+import { PeronsService } from '../services/perons.service';
+import { Person } from '../shared/model/person';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-choose-team-dialog',
   standalone: true,
   imports: [
-    CommonModule, MatDialogModule, MatButtonModule
+    CommonModule, MatDialogModule, MatButtonModule, MatIconModule, MatProgressBarModule
   ],
   templateUrl: './choose-team-dialog.component.html',
   styleUrl: './choose-team-dialog.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ChooseTeamDialogComponent { 
-  constructor(@Inject(MAT_DIALOG_DATA) public selectedTask: Task){}
+export class ChooseTeamDialogComponent implements OnInit { 
+  allPersons : Person[] = [];
+  randomTeam : Person[] = [];
+  personIndex = 0;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public selectedTask: Task,
+    private personService : PeronsService){}
+
+  ngOnInit(): void {
+    this.allPersons = this.personService.list();
+    this.generateRandomTeam();
+  }
+
+  generateRandomTeam() {
+    this.randomTeam = [...this.allPersons];
+    this.randomTeam.sort(() => Math.random() - 0.5);
+    this.randomTeam = this.randomTeam.slice(0, Math.min(4, this.allPersons.length));
+  }
+
+  nextPerson() {
+    ++this.personIndex;
+
+    if (this.personIndex == this.randomTeam.length) {
+      //close dialog
+    }
+  }
 }
